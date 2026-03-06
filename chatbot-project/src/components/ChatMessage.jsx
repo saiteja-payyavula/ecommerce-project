aee578a04f0e42f7aa49ced05c9856e85253c6f9
saiteja-payyavula <saitejapayyavula123@gmail.com>
2026-03-06 13:46:10 +0530

    Add the starting files

---
+++ chatbot-project/src/components/ChatMessage.jsx
@@ -0,0 +1,33 @@
+import RobotprofileImage from "../assets/robot.png";
+import UserprofileImage from "../assets/user.png";
+import "./ChatMessage.css";
+
+export function ChatMessage({ message, sender }) {
+  //const message = props.message;
+  //const sender = props.sender;
+  //const {message,sender} = props;
+  /*
+  if (sender === "robot") {
+    return (
+      <div>
+        <img src="robot.png" width="50" />
+        {message}
+      </div>
+    );
+  }*/
+
+  return (
+    <div
+      className={sender === "user" ? "chat-message-user" : "chat-message-robot"}
+    >
+      {sender === "robot" && (
+        <img src={RobotprofileImage} className="chat-message-profile" />
+      )}
+
+      <div className="chat-message-text">{message}</div>
+      {sender === "user" && (
+        <img src={UserprofileImage} className="chat-message-profile" />
+      )}
+    </div>
+  );
+}
