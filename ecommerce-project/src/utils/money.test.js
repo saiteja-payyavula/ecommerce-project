8cc20cd483529cb2e1163511a9ad5f37f870a5dd
saiteja-payyavula <saitejapayyavula123@gmail.com>
2026-03-12 13:39:45 +0530

    Create integration test for Product component

--- ecommerce-project/src/utils/money.test.js
+++ ecommerce-project/src/utils/money.test.js
@@ -6,7 +6,7 @@ describe("formtMoney", () => {
     expect(formatMoney(1999)).toBe("$19.99");
   });
 
-  it("displays @ decimals", () => {
+  it("displays 2 decimals", () => {
     expect(formatMoney(1090)).toBe("$10.90");
     expect(formatMoney(100)).toBe("$1.00");
   });
