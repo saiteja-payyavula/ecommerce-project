006f7d399257333ce4a33d564727f824e697cf6f
saiteja-payyavula <saitejapayyavula123@gmail.com>
2026-03-31 14:54:45 +0530

    Delete assets

--- ecommerce-project/src/utils/money.test.js
+++ ecommerce-project/src/utils/money.test.js
@@ -1,7 +1,7 @@
 import { it, expect, describe } from "vitest";
 import { formatMoney } from "./money";
 
-describe("formtMoney", () => {
+describe("formatMoney", () => {
   it("formats 1999 cents as $19.99", () => {
     expect(formatMoney(1999)).toBe("$19.99");
   });
